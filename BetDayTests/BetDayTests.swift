8c7c40a56006c1862de012487312286aac25814f
jhonatan9170 <65328388+jhonatan9170@users.noreply.github.com>
2026-03-18 16:25:06 -0500

    Initial Commit

---
+++ BetDayTests/BetDayTests.swift
@@ -0,0 +1,17 @@
+//
+//  BetDayTests.swift
+//  BetDayTests
+//
+//  Created by Jhonatan Chavez  on 18/03/26.
+//
+
+import Testing
+@testable import BetDay
+
+struct BetDayTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
